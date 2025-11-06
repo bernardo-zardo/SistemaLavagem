dd2fc4c577d5abfdae04dea4938b86eb8eac260c
bernardo zardo mergen <bernardozardomergen@gmail.com>
2025-11-05 23:03:22 -0300

    Projeto TCC
    - Alteracao nos graficos.

--- src/main/java/com/bernardo/beans/DashboardBean.java
+++ src/main/java/com/bernardo/beans/DashboardBean.java
@@ -2,107 +2,102 @@ package com.bernardo.beans;
 
 import java.io.Serializable;
 import java.util.Arrays;
+
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
 import org.primefaces.model.charts.ChartData;
+import org.primefaces.model.charts.axes.cartesian.CartesianScales;
+import org.primefaces.model.charts.axes.cartesian.linear.CartesianLinearAxes;
 import org.primefaces.model.charts.bar.BarChartDataSet;
 import org.primefaces.model.charts.bar.BarChartModel;
 import org.primefaces.model.charts.bar.BarChartOptions;
-import org.primefaces.model.charts.axes.cartesian.CartesianScales;
-import org.primefaces.model.charts.axes.cartesian.linear.CartesianLinearAxes;
 import org.primefaces.model.charts.donut.DonutChartDataSet;
 import org.primefaces.model.charts.donut.DonutChartModel;
 import org.primefaces.model.charts.donut.DonutChartOptions;
-import org.primefaces.model.charts.optionconfig.legend.Legend;
 import org.primefaces.model.charts.optionconfig.title.Title;
 
 @Named
 @ViewScoped
 public class DashboardBean implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private BarChartModel barModel;
-	private DonutChartModel donutModel;
+    private DonutChartModel donutModel;
+    private BarChartModel barModel;
 
-	@PostConstruct
-	public void init() {
-		createBarModel();
-		createDonutModel();
-	}
+    @PostConstruct
+    public void init() {
+        createDonutModel();
+        createBarModel();
+    }
 
-	private void createBarModel() {
-		barModel = new BarChartModel();
-		ChartData data = new ChartData();
+    private void createDonutModel() {
+        donutModel = new DonutChartModel();
+        ChartData data = new ChartData();
 
-		BarChartDataSet ganhos = new BarChartDataSet();
-		ganhos.setLabel("Ganhos Financeiros (R$)");
-		ganhos.setBackgroundColor("rgba(54, 162, 235, 0.6)");
-		ganhos.setBorderColor("rgb(54, 162, 235)");
-		ganhos.setBorderWidth(1);
-		ganhos.setData(Arrays.asList(1200, 900, 1500, 2000, 1800, 2100, 2500, 2300, 2600, 3000, 2800, 3200));
+        DonutChartDataSet dataSet = new DonutChartDataSet();
+        dataSet.setData(Arrays.asList(120, 90, 50));
+        dataSet.setBackgroundColor(Arrays.asList("#42A5F5", "#66BB6A", "#FFA726"));
+        data.addChartDataSet(dataSet);
 
-		BarChartDataSet servicos = new BarChartDataSet();
-		servicos.setLabel("Serviços Realizados");
-		servicos.setBackgroundColor("rgba(255, 99, 132, 0.6)");
-		servicos.setBorderColor("rgb(255, 99, 132)");
-		servicos.setBorderWidth(1);
-		servicos.setData(Arrays.asList(40, 35, 45, 50, 48, 52, 60, 58, 63, 70, 68, 75));
+        data.setLabels(Arrays.asList("Lavagem simples", "Lavagem completa", "Polimento"));
+        donutModel.setData(data);
 
-		data.addChartDataSet(ganhos);
-		data.addChartDataSet(servicos);
-		data.setLabels(
-				Arrays.asList("Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"));
-		barModel.setData(data);
+        DonutChartOptions options = new DonutChartOptions(); 
 
-		BarChartOptions options = new BarChartOptions();
-		Title title = new Title();
-		title.setDisplay(true);
-		title.setText("Ganhos e Serviços - Últimos 12 Meses");
-		options.setTitle(title);
+        Title title = new Title();
+        title.setDisplay(true);
+        title.setText("Serviços por Tipo");
+        options.setTitle(title);
+        
+        donutModel.setOptions(options);
+    }
 
-		Legend legend = new Legend();
-		legend.setDisplay(true);
-		legend.setPosition("top");
-		options.setLegend(legend);
+    private void createBarModel() {
+        barModel = new BarChartModel();
+        ChartData data = new ChartData();
 
-		CartesianScales scales = new CartesianScales();
-		CartesianLinearAxes yAxis = new CartesianLinearAxes();
-		yAxis.setBeginAtZero(true);
-		scales.addYAxesData(yAxis);
-		options.setScales(scales);
+        BarChartDataSet ganhosDataSet = new BarChartDataSet();
+        ganhosDataSet.setLabel("Ganhos (R$)");
+        ganhosDataSet.setBackgroundColor("#42A5F5");
+        ganhosDataSet.setData(Arrays.asList(1200, 1500, 800, 1800, 2000, 2200, 1700, 2500, 2300, 2100, 2600, 2800));
 
-		barModel.setOptions(options);
-	}
+        BarChartDataSet servicosDataSet = new BarChartDataSet();
+        servicosDataSet.setLabel("Serviços");
+        servicosDataSet.setBackgroundColor("#66BB6A");
+        servicosDataSet.setData(Arrays.asList(45, 55, 38, 60, 70, 65, 58, 75, 72, 68, 80, 90));
 
-	private void createDonutModel() {
-		donutModel = new DonutChartModel();
-		ChartData data = new ChartData();
+        data.addChartDataSet(ganhosDataSet);
+        data.addChartDataSet(servicosDataSet);
+        data.setLabels(Arrays.asList("Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"));
 
-		DonutChartDataSet dataSet = new DonutChartDataSet();
-		dataSet.setData(Arrays.asList(120, 90, 60, 30));
-		dataSet.setBackgroundColor(Arrays.asList("#36A2EB", "#FF6384", "#FFCE56", "#4BC0C0"));
-		data.addChartDataSet(dataSet);
+        barModel.setData(data);
 
-		data.setLabels(Arrays.asList("Lavagem Simples", "Lavagem Completa", "Polimento", "Higienização Interna"));
-		donutModel.setData(data);
+        // opções do gráfico de barras
+        BarChartOptions options = new BarChartOptions();
 
-		DonutChartOptions options = new DonutChartOptions();
-		Title title = new Title();
-		title.setDisplay(true);
-		title.setText("Serviços Realizados por Tipo");
-		options.setTitle(title);
+        CartesianScales scales = new CartesianScales();
+        CartesianLinearAxes yAxis = new CartesianLinearAxes();
+        yAxis.setBeginAtZero(true);
+        scales.addYAxesData(yAxis);
+        options.setScales(scales);
 
-		donutModel.setOptions(options);
-	}
+        Title title = new Title();
+        title.setDisplay(true);
+        title.setText("Ganhos e Serviços dos Últimos 12 Meses");
+        options.setTitle(title);
 
-	public BarChartModel getBarModel() {
-		return barModel;
-	}
+        barModel.setOptions(options);
+    }
 
-	public DonutChartModel getDonutModel() {
-		return donutModel;
-	}
+    // getters
+    public DonutChartModel getDonutModel() {
+        return donutModel;
+    }
+
+    public BarChartModel getBarModel() {
+        return barModel;
+    }
 }
